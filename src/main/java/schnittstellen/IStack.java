8d8f15a5886a50dc5fe7b2b599f4042b13cf8a06
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-12-30 18:03:38 +0100

    feat: add Stack implementation

---
+++ src/main/java/schnittstellen/IStack.java
@@ -0,0 +1,18 @@
+package schnittstellen;
+
+public interface IStack {
+    public IList getDVL();
+
+    public int getSize();
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+
+    public int pop();
+
+    public void push(int value);
+
+    public int top();
+
+}
