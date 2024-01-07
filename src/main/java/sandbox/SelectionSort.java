f4634641baf94dd579e754d9a60a7d9a0c7ff5c3
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-01-07 12:13:00 +0100

    feat: add SelectionSort

---
+++ src/main/java/sandbox/SelectionSort.java
@@ -0,0 +1,35 @@
+package sandbox;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+
+    public static int[] sort(int[] arr) {
+
+        int steps = 0, min, tmp;
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+
+            // find the minimum element in the remaining array
+            // relative to index i
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+                steps++;
+            }
+
+            // change the elements, i becomes the new minimum
+            tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+
+        }
+
+
+        return arr;
+    }
+
+
+}
