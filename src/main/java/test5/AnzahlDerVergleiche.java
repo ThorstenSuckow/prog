2cbd2e88ec36069e87231fe59c7d35a0d6803773
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-01-17 22:10:02 +0100

    refactor: update test implementation for ShellSort

--- src/main/java/test5/AnzahlDerVergleiche.java
+++ src/main/java/test5/AnzahlDerVergleiche.java
@@ -19,6 +19,7 @@ public class AnzahlDerVergleiche {
         while (distanz > 0) {                           // lg n
             for (i = distanz; i < feld.length; i++) {   // n / 2
                 j = i - distanz;
+                count++;
                 while (j >= 0) {                        // n / 2
                     if (feld[j] > feld[j + distanz]) {
                         swap(feld, j, j + distanz);
@@ -26,52 +27,72 @@ public class AnzahlDerVergleiche {
                     } else {
                         j=-1;
                     }
-                    count++;
+
                 }
             }
             distanz = distanz / 2;
         }
 
-        System.out.println("COUNT A: " + count);
+        System.out.println("COUNT A: " + count + ", " + (count/ (double)n));
         return feld;
     }
 
 
     public static int[] second(int[] feld) {
 
+        System.out.println(Arrays.toString(feld));
+
         int i, j;
         int temp;
         int n = feld.length;
-        int distanz = n / 2;
+        int distanz = n/2;
+
+        int innerCount = 0;
+        int whileCount = 0;
 
         int count = 0;
+        int swaps = 0;
         System.out.println("N: " + n);
         while (distanz > 0)                                      // lg n
         {
-            System.out.println("DISTANZ: " + distanz);
+            whileCount = 0;
+            int loopSwaps = 0;
             for (i = distanz; i < feld.length; i++)
             {
                 j =  i - distanz;
 
-                while (j >= 0) { //
+                count++;
+                innerCount++;
+                while (j >= 0){// && feld[j] > feld[j + distanz]) { //
+
+
+                    whileCount++;
+
+                    System.out.println("[incr] " + distanz +"; i,j: [" + i + "," +j + "] ->          " + Arrays.toString(feld));
+
                     if (feld[j] > feld[j + distanz]) { // n
+                        System.out.print("   [swap]       [" + j +", " + (j + distanz) +"]; j: " + (j - distanz));
+                        swaps++;
+                        innerCount++;
+                        loopSwaps++;
                         //Vertauschen der beiden Feldwerte
                         temp = feld[j];
                         feld[j] = feld[j + distanz];
                         feld[j + distanz] = temp;
                         j = j - distanz;
+                        System.out.println( " -> " + Arrays.toString(feld) );
                     } else {
-                        break;
+                        j = -1;
                     }
-                    count++;
-
                 }
-
              }
+            System.out.println(" -- loop swaps: " + loopSwaps);
+            System.out.println(" -- while count: " + whileCount);
+            System.out.println();
             distanz = distanz / 2;
         }
 
-        System.out.println("COUNT B: " + count);
+        System.out.println("COUNT B - total:" + innerCount +"; count: "+ count+";  while: " + (innerCount - count)+ "; swaps: " + swaps);
         return feld;
     }
 
--- src/test/java/test5/AnzahlDerVergleicheTest.java
+++ src/test/java/test5/AnzahlDerVergleicheTest.java
@@ -17,7 +17,7 @@ public class AnzahlDerVergleicheTest {
         //int[] f1 = new int[]{92, 1, 23, 2, 93, 245, 32, 235325, 523, 32, 44, 55, 22, 11};
         //int[] f2 = new int[]{92, 1, 23, 2, 93, 245, 32, 235325, 523, 32, 44, 55, 22, 11};
 
-        int l = 256;
+        int l = 16;
         int[] f1 = new int[l];
         int[] f2 = new int[l];
 
