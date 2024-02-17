4bdf9fe717ad2fdf3163136e23f0237edafe44c5
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-02-17 11:53:57 +0100

    docs: add chapter Exceptions to Repetitorium

---
+++ src/main/java/sandbox/ExceptionsDemo.java
@@ -0,0 +1,66 @@
+package sandbox;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.sql.SQLClientInfoException;
+
+public class ExceptionsDemo {
+
+    abstract class AbstractExceptionThrower {
+
+        void foo() throws IOException {
+            throw new IOException();
+        }
+
+        abstract void fooAbstract() throws RemoteException;
+    }
+
+    class BaseExceptionThrower extends AbstractExceptionThrower {
+        void foo() {
+            super.foo();
+        }
+
+        void fooAbstract() {
+
+        }
+
+
+    }
+
+    interface Exceptionable {
+        void createClient() throws RemoteException;
+    }
+    static class ExceptionImpl implements Exceptionable {
+
+        @Override
+        public void createClient() throws IOException {
+            // Implementierung benutzt keine
+            // Methode, die eine IOException werfen kann
+            throw new IOxception();
+        }
+    }
+
+    private void writeFile() throws IOException {
+        File f = new File("./datei.txt");
+        f.createNewFile();
+    }
+
+    private void runtimeThrow() throws RuntimeException {
+        throw new RuntimeException();
+    }
+
+    private void runtimeThrowCaller() {
+        runtimeThrow();
+    }
+
+
+    public static void main (String[] args) {
+
+        new ExceptionImpl();
+
+    }
+
+}
