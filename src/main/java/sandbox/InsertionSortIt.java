88783f5426afbd5166ab4d0c926b8dcb7baa0d57
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-03-10 15:36:23 +0100

    docs: add section about Insertion-Sort

--- src/main/java/sandbox/InsertionSortIt.java
+++ src/main/java/sandbox/InsertionSortIt.java
@@ -5,12 +5,13 @@ public class InsertionSortIt {
 
     public static int[] sort(int[] arr) {
 
-
+        int vergleiche = 0;
         for (int i = 1; i < arr.length; i++) {
 
             int min = arr[i];
             int n = i;
             while (n > 0 && arr[n - 1] > min) {
+                vergleiche++;
                 arr[n] = arr[n - 1];
                 n--;
             }
@@ -18,7 +19,7 @@ public class InsertionSortIt {
             arr[n] = min;
 
         }
-
+        System.out.println(vergleiche);
         return arr;
 
     }
